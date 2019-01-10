633ca755f7940e07df83e9be2f9462eea7c8c2fd
nurpariz <nurpariz@nurparizs-MacBook-Pro.local>
2019-01-10 07:22:35 +0700

    Initial Commit

---
+++ SchotersTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SchotersTest
+//
+//  Created by nurpariz on 10/01/19.
+//  Copyright © 2019 nurpariz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
